2874e837d1d67ebceb057fc75bdcfdd26b6c5bee
Prajeet Shah <prajeetshah93@gmail.com>
2025-03-13 14:13:39 +0545

    Multiple Route Handlers

--- src/app.js
+++ src/app.js
@@ -2,17 +2,27 @@ const express = require("express");
 
 const app = express();
 
-app.use("/userApi", (req, res) => {
-  res.send("hello world from userAPIs");
-});
-
-app.use("/test", (req, res) => {
-  res.send("hello world from test");
-});
-
-app.use((req, res) => {
-  res.send("hello world");
-});
+app.use("/user", [
+  (req, res, next) => {
+    console.log("response 1");
+    // res.send("hello world");
+    next();
+  },
+  (req, res, next) => {
+    console.log("response 2");
+    res.send("hello world 2");
+  },
+  (req, res, next) => {
+    console.log("response 3");
+    res.send("hello world 3");
+    next();
+  },
+  (req, res, next) => {
+    console.log("response 4");
+    res.send("hello world 4");
+    next();
+  },
+]);
 
 app.listen(7777, () => {
   console.log("server running on the port 7777");

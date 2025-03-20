f9ee01f2f242e36f29a0c32937bb936ec1e938c9
Prajeet Shah <prajeetshah93@gmail.com>
2025-03-20 22:18:34 +0545

    Forgot password APIs

--- src/routes/profile.js
+++ src/routes/profile.js
@@ -1,6 +1,8 @@
 const express = require("express");
 const { AuthUser } = require("../middlewares/Auth");
 const User = require("../models/user");
+const validator = require("validator");
+const bcrypt = require("bcrypt");
 const { validateProfileEditData } = require("../utils/validation");
 
 const profileRouter = express.Router();
@@ -43,9 +45,49 @@ profileRouter.patch("/profile/edit", AuthUser, async (req, res) => {
       throw new Error("something went wrong");
     }
 
-    res.json({ message: "profile updated successfully", data: updatedProfile });
+    res.json({
+      message: ` ${
+        updatedProfile.firstName + updatedProfile.lastName
+      } profile updated successfully`,
+      data: updatedProfile,
+    });
   } catch (err) {
     res.status(400).send("ERROR : " + err.message);
+  }
+});
+
+profileRouter.patch("/profile/password", AuthUser, async (req, res) => {
+  // enter the previous password
+  try {
+    const { oldPassword, newPassword } = req.body;
+
+    if (!oldPassword || !newPassword) {
+      return res.json({ message: "enter the new and old password" });
+    }
+
+    const user = req.user;
+
+    const IsOlderPasswordVerify = await bcrypt.compare(
+      oldPassword,
+      user.password
+    );
+    if (!IsOlderPasswordVerify) {
+      return res.json({ message: "please enter the correct old password" });
+    }
+
+    const IspasswordValidate = validator.isStrongPassword(newPassword);
+    if (!IspasswordValidate) {
+      throw new Error("please enter the strong password");
+    }
+    const passwordHash = await bcrypt.hash(newPassword, 10);
+    await User.findByIdAndUpdate(
+      user._id,
+      { password: passwordHash },
+      { new: true }
+    );
+    res.json({ message: "password updated successfully" });
+  } catch (err) {
+    res.status(400).json({ error: `ERROR : ${err.message}` });
   }
 });
 
